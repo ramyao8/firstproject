fa4667343ea11a844eca613cef44653592a638ba
ramyao8 <150414710+ramyao8@users.noreply.github.com>
2023-11-10 11:06:02 +0530

    first commit
    calref

---
+++ callbyrefer.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int add(int &b)
+{
+	cout<<"the values before increment in sub function is "<<b<<endl;
+	b=b+50;
+	cout<<"the values after increment in subfunction is "<<b<<endl;
+	return b;
+}
+int main()
+{
+	int a,b;
+	cout<<"enter a";
+	cin>>a;
+	cout<<"the value of a before increment in main function is "<<a<<endl;
+	add(a);
+	cout<<"the value of a after increment in main function is "<<a<<endl;
+}
+
